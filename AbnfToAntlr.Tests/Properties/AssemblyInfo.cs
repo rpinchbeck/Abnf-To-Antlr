c861e81b9f5010d9841961bc2bc233c664f59191
rpinchbeck <5355465+rpinchbeck@users.noreply.github.com>
2018-04-28 10:45:09 +1200

    Include uppercase and lowercase characters in distinct character collection for char-val nodes.  Add unit tests for char-val nodes.

---
+++ AbnfToAntlr.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,57 @@
+﻿/*
+
+    Copyright 2012-2013 Robert Pinchbeck
+  
+    This file is part of AbnfToAntlr.
+
+    AbnfToAntlr is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AbnfToAntlr is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AbnfToAntlr.  If not, see <http://www.gnu.org/licenses/>.
+  
+*/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AbnfToAntlr.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Robert Pinchbeck")]
+[assembly: AssemblyProduct("AbnfToAntlr")]
+[assembly: AssemblyCopyright("Copyright © 2012-2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("071c365e-59e1-4ad2-9c39-59383a01996f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
---
+++ AbnfToAntlr.Tests/TestBase.cs
@@ -0,0 +1,56 @@
+﻿/*
+
+    Copyright 2018 Robert Pinchbeck
+  
+    This file is part of AbnfToAntlr.
+
+    AbnfToAntlr is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AbnfToAntlr is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AbnfToAntlr.  If not, see <http://www.gnu.org/licenses/>.
+  
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbnfToAntlr.Tests
+{
+    /// <summary>
+    /// Creates a TestContext property for derived test classes
+    /// </summary>
+    public abstract class TestBase : IDisposable
+    {
+        public TestContext TestContext { get; set; } // automatically bound to the current test context at test time
+
+        #region IDisposable Support
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // do nothing, allow derived classes to override dispose method
+        }
+
+        ~TestBase()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
